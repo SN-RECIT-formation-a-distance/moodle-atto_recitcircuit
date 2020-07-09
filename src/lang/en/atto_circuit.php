2cb5075896f26f2d9042068f97495f529426dc3b
Étienne <etienne.roy@recitad.ca>
2020-07-09 06:28:26 -0400

    premier premier commit

---
+++ src/lang/en/atto_circuit.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_circuit', language 'en'.
+ *
+ * @package    atto_circuit
+ * @copyright  2013 Damyon Wiese  <damyon@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Circuit';
+$string['privacy:metadata'] = 'The Circuit plugin does not save or export user data.';
+$string['circuittitle'] = 'Circuit and Save';
+$string['insert'] = 'Insert Circuit';
+$string['settings'] = 'Circuit settings';
+$string['storeinrepo'] = 'Store as image file';
+$string['storeinrepo_desc'] = 'If checked the circuit will be saved as a standard image upload instead of a base64 inline image.';
---
+++ src/lang/fr_ca/atto_circuit.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_circuit', language 'en'.
+ *
+ * @package    atto_circuit
+ * @copyright  2013 Damyon Wiese  <damyon@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Circuit';
+$string['privacy:metadata'] = 'Le plugin Circuit n\'importe pas ou n\'exporte pas de données utilisateurs.';
+$string['circuittitle'] = 'Dessiner votre circuit et enregistrer';
+$string['insert'] = 'Inserer un Circuit';
+$string['settings'] = 'Paramètres  de Circuit';
+$string['storeinrepo'] = 'Enregistrer comme un fichier image';
+$string['storeinrepo_desc'] = 'Si cette case est cochée, le circuit sera enregisté comme une inage standard à la place d\'une  image en ligne base64 .';
