5bf85a85be9f14c80ed6e5a910c4d99d52b4672e
Émile Jacques <emilevirus@gmail.com>
2022-10-05 12:05:53 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022100100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = 'v1.15.1-stable';
-$plugin->supported = [39, 311];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported. 
+$plugin->supported = [39, 400];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported. 
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'atto_circuit';
 $plugin->maturity = MATURITY_STABLE;
