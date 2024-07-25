423b2b33dd755f67b612511fdac4bfb870d3f7ec
Gustavo Bazzo <gcbazzo@gmail.com>
2024-07-25 14:20:50 -0400

    Upgrade Moodle 4.5

--- src/version.php
+++ src/version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024050900;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 'v1.16.4-stable';
-$plugin->supported = [39, 403];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported. 
+$plugin->version   = 2024072500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 'v1.16.5-stable';
+$plugin->supported = [39, 405];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported. 
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'atto_circuit';
 $plugin->maturity = MATURITY_STABLE;
